b59ec2d72f8c0731f48b9dd5049f9d58ea76e25e
cheonhobeom <14452@deu.ac.kr>
2020-03-05 09:33:04 +0900

    swagger 기능 추가 접속 URL : localhost:8080/swagger-ui.html

--- api-server/src/main/java/com/mukflix/backend/apiserver/ApiServerApplication.java
+++ api-server/src/main/java/com/mukflix/backend/apiserver/ApiServerApplication.java
@@ -3,9 +3,11 @@ package com.mukflix.backend.apiserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Map;
 
+@EnableSwagger2
 @SpringBootApplication
 public class ApiServerApplication {
 
