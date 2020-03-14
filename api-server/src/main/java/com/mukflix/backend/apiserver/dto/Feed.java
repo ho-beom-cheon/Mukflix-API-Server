f0ef8eeb5c796064cac676f33b6ef73f25e64fe6
이현승 <newsforlemon@gmail.com>
2020-03-14 13:52:56 +0900

    [MAIN-0001] feed - 식사기록 기능   - 기본 뼈대 생성 및 기능 정의   - Feed 객체 데이터 구조 생성

--- api-server/src/main/java/com/mukflix/backend/apiserver/dto/Feed.java
+++ api-server/src/main/java/com/mukflix/backend/apiserver/dto/Feed.java
@@ -2,11 +2,36 @@ package com.mukflix.backend.apiserver.dto;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Entity
 @Table(name = "USR_MEAL_INFO")
 public class Feed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long feedId;
+
+    private Long userId;
+
+    private Date feedDatetime;
+
+    private String placeCode;
+
+    private Long restaurantId;
+
+    private Long meneId;
+
 }
+
+/** 실제 테이블 생성 쿼리문
+ * CREATE TABLE USR_MEAL_INFO(
+ *     MEAL_INFO_SQ   VARCHAR(10)   NOT NULL
+ *   , USR_SQ     VARCHAR(40)   NOT NULL
+ *   , MEAL_DDTM   VARCHAR(14)   NOT NULL
+ *   , PLACE_CD     VARCHAR(2)     DEFAULT '01'
+ *   , RESTAURANT_SQ VARCHAR(10)
+ *   , MENU_SQ     VARCHAR(10)
+ *   , PRIMARY KEY(MEAL_INFO_SQ)
+ */
